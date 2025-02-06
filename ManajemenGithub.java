e96e19694137eea1315c447b79e27cc33640d856
Talitha-Ss2 <talithassalsabila16@gmail.com>
2025-02-06 12:56:14 +0700

    Create ManajemenGithub.java

---
+++ ManajemenGithub.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package manajemengithub;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ManajemenGithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hai! Saya Talitha Syahla Salsabila dengan NIM E41240073 Golongan A");
+    }
+    
+}
